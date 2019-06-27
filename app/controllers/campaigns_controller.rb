26c3177f0a471b2df176e3c0a7e284bc2d3adc36
watila <watila.costa@fortaleza.ce.gov.br>
2019-06-27 16:46:29 -0300

    Criando controllers e mailer iniciais

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,20 @@
+class CampaignsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  # Sorteio do amigo secreto
+  def raffle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'campaigns/show'
+  get 'campaigns/index'
+  get 'campaigns/create'
+  get 'campaigns/update'
+  get 'campaigns/destroy'
+  get 'campaigns/raffle'
+  get 'pages/home'
+  get 'members/crate'
+  get 'members/destroy'
+  get 'members/update'
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   # Incluir aqui verificação se user é administrador
