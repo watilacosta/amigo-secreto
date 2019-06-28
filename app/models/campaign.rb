138af062473ab2383bbb5a460d217cb7570e2d5f
watila <watila.costa@fortaleza.ce.gov.br>
2019-06-28 16:38:33 -0300

    Criando métodos principais dos models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,19 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
+
+  before_create :set_member
+  before_create :set_status
+
+  enum status: [:pending, :finished]
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending
+  end
+
+  # Quem está criando a campaign também é membro desta
+  def set_member
+    self.members = << Member.create(name: self.user.name, email: self.user.email)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,22 @@
 class Member < ApplicationRecord
   belongs_to :campaign
+  after_save :set_campaign_pending
+  validates  :name, :campaign, presence: true
+
+  def set_pixel
+    self.open  = false
+    # Gera um token enquanto ele seja único, não exista
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token if not Member.exists?(token: random_token)
+    end
+    self.save!
+  end
+
+  protected
+
+  # Sempre que um membro é adicionando a uma campaign, a mesma é atualizada para status pending
+  def set_campaign_pending
+    self.campaign.update(status: :pending)
+  end
 end
