73ac02672e89722b1c96c27938f27c0e55abf1ab
watila <watila.costa@fortaleza.ce.gov.br>
2019-08-07 11:40:14 -0300

    config app para deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,6 @@ Rails.application.configure do
 
   # Mailcatcher
   # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+  config.action_mailer.delivery_method     = :smtp
+  config.action_mailer.smtp_settings       = { :address => 'mailcatcher', :port => 1025 }
 end
