a012ece4bd31bb515b6c64f407b3f9d9dbbe1204
watila <watila.costa@fortaleza.ce.gov.br>
2019-08-07 11:41:43 -0300

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Mailcatcher
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method     = :smtp
   config.action_mailer.smtp_settings       = { :address => 'mailcatcher', :port => 1025 }
 end
